51a737f9ea4619d502843e02d76bc585d52ca861
vaskivstanislav <stasmegatyr@gmail.com>
2025-01-28 19:37:01 +0200

    s

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  }
+  base: "/goit-react-hw-04/", 
+  plugins: [react()],
+  build: {
+    outDir: "dist", 
+    emptyOutDir: true, 
+  },
 });
